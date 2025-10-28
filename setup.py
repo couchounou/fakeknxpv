0d9ce3de3baceaca32599f9476f9ff069a645f64
couchounou <couchounou@hotmail.com>
2025-10-28 10:39:41 +0100

    Bump version to 0.3.1 in package metadata and update setup files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fake_knx_pv",
-    version="0.2.0",
+    version="0.3.1",
     author="Eric",
     description="Simulation KNX PV pour données de production et consommation.",
     packages=find_packages(),
