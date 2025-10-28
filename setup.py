65bc453b9a7fee897f0594647e1eb414644ca90c
couchounou <couchounou@hotmail.com>
2025-10-28 07:52:08 +0100

    Bump version to 0.2.0 and update author name in package metadata

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name="fake_knx_pv",
-    version="0.1.0",
-    author="VotreNom",
+    version="0.2.0",
+    author="Eric",
     description="Simulation KNX PV pour données de production et consommation.",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[],
+    install_requires=[
+        "xknx",
+        "pytz",
+        "astral",
+        "requests"
+    ],
     python_requires='>=3.7',
 )
