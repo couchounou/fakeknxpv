041f2d3e5fae851c8b3504fc17460d4fe5283c6c
couchounou <couchounou@hotmail.com>
2025-10-31 15:04:20 +0100

    fix: Update version to 0.2.5 in multiple files and adjust service dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fake_knx_pv",
-    version='0.2.4',
+    version='0.2.5',
     author="Eric",
     description="Simulation KNX PV pour données de production et consommation.",
     packages=find_packages(),
