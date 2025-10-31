5a124a0cbe93edb0cb6c215fb692f10bdc018f29
couchounou <couchounou@hotmail.com>
2025-10-31 09:38:01 +0100

    fix: Move service restart command to the correct position in git-update script

--- services/git-update.sh
+++ services/git-update.sh
@@ -3,6 +3,6 @@
 cd /home/pi/fakeknxpv/ || exit 1
 /usr/bin/git pull
 echo "Updating git-update.sh script and restarting service..."
+sudo systemctl restart fakeknxpv
 sudo cp /home/pi/fakeknxpv/services/git-update.sh /home/pi/
 sudo chmod +x /home/pi/git-update.sh
-sudo systemctl restart fakeknxpv
