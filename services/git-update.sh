d594386fffa8fa4a6b7fc3ae55b44d828db77ffb
couchounou <couchounou@hotmail.com>
2025-10-31 09:22:57 +0100

    fix: Correct path in chmod command in git-update script

--- services/git-update.sh
+++ services/git-update.sh
@@ -2,5 +2,5 @@
 
 cd /home/pi/fakeknxpv/ || exit 1
 /usr/bin/git pull
-sudo chmod +x /home/pi/fakeknxpv/fake_knx_pv/services/git-update.sh
+sudo chmod +x /home/pi/fakeknxpv/services/git-update.sh
 sudo systemctl restart fakeknxpv
