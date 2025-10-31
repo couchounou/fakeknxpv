cc601bccee8dd9d2b8ac39b5bcbb2137b012d7a6
couchounou <couchounou@hotmail.com>
2025-10-31 12:24:45 +0100

    feat: Revamp installation script and update service dependencies

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd /home/pi/ || exit 1
+sudo apt-get install -y git
+git clone https://github.com/couchounou/fakeknxpv.git
+sudo apt-get install -y python3-pip
+python3 -m venv ./venv
+source venv/bin/activate
+pip install /home/pi/fakeknxpv/dist/fake_knx_pv-0.2.4-py3-none-any.whl
+deactivate
+sudo cp /home/pi/fakeknxpv/services/git-update.sh /home/pi/
+sudo cp /home/pi/fakeknxpv/services/gitupdate.service /etc/systemd/system/
+sudo cp /home/pi/fakeknxpv/services/fakeknxpv.service /etc/systemd/system/
+sudo chmod +x /home/pi/git-update.sh
+sudo cp ./fakeknxpv/fake_knx_pv/cyclic_send_toknx_pv_data.cfg /boot/firmware/
+# sudo touch /boot/firmware/index_inject.txt
+# sudo touch /boot/firmware/index_sout.txt
+# sudo touch /boot/firmware/index_conso.txt
+# sudo touch /boot/firmware/index_prod.txt
+# sudo chown pi:pi /boot/firmware/index_*.*
+sudo chmod +r /boot/firmware/index_*.*
+sudo systemctl daemon-reload
+sudo systemctl enable gitupdate.service
+sudo systemctl start gitupdate.service
+sudo systemctl enable fakeknxpv.service
+sudo systemctl start fakeknxpv.service
+echo "Installation complete."
