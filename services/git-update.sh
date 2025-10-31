e978b7ab0f74eed7ec090be859d7cf9089803c70
couchounou <couchounou@hotmail.com>
2025-10-31 09:31:09 +0100

    fix: Add echo statement to indicate script update and service restart

--- services/git-update.sh
+++ services/git-update.sh
@@ -2,6 +2,7 @@
 
 cd /home/pi/fakeknxpv/ || exit 1
 /usr/bin/git pull
+echo "Updating git-update.sh script and restarting service..."
 sudo cp /home/pi/fakeknxpv/services/git-update.sh /home/pi/
 sudo chmod +x /home/pi/git-update.sh
 sudo systemctl restart fakeknxpv
