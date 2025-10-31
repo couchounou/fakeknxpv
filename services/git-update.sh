d9e5a0bb7dd535c18b75b22e96acf481cff252d3
couchounou <couchounou@hotmail.com>
2025-10-31 09:19:07 +0100

    fix: Add missing permissions and restart command in git-update script

--- services/git-update.sh
+++ services/git-update.sh
@@ -2,4 +2,5 @@
 
 cd /home/pi/fakeknxpv/ || exit 1
 /usr/bin/git pull
-systemctl restart fakeknxpv
+sudo chmod +x /home/pi/fakeknxpv/fake_knx_pv/services/git-update.sh
+sudo systemctl restart fakeknxpv
