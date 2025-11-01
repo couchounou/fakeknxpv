8568df52ea697b61864c36489dd4ef3d99c2afaa
couchounou <couchounou@hotmail.com>
2025-11-01 09:36:02 +0100

    fix: Update save cycle duration and adjust occupancy detection threshold

--- fake_knx_pv/cyclic_send_toknx_pv_data.py
+++ fake_knx_pv/cyclic_send_toknx_pv_data.py
@@ -430,7 +430,7 @@ async def send_power_data(
 
                 # occupancy detection
                 occupancy_state = False
-                if conso_w < (household_power * 0.1):
+                if conso_w < (household_power * 0.08):
                     occupancy_state = False
                 else:
                     occupancy_state = True
